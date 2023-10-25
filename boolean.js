905370a22ce65ddc53771621be9afbd68629a8a8
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-25 17:17:54 +0300

    add: search weekday

---
+++ boolean.js
@@ -0,0 +1,11 @@
+const firstNumber =6
+const secondNumber = 6
+if (firstNumber<secondNumber)
+console.log  ("число " + firstNumber + " меньше " + secondNumber)
+else if (firstNumber === secondNumber)
+console.log  ( "данные числа равны ")
+else
+console.log  ("число " + firstNumber + " больше " + secondNumber)
+
+
+
