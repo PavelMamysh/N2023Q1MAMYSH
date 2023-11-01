e8ed82a124c6887d454cfb972cb55b2b9488b4df
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-11-01 16:57:24 +0300

    add loop.js

---
+++ loop.js
@@ -0,0 +1,12 @@
+let count = 1
+for(i = 3; i<10 ;i++){
+    console.log(`на ${count} итерации число ${i}`)
+    count++
+}
+
+let schetchik = 1
+for(i=5647; i<5901;i++){
+    if (i%2===0)
+    console.log  (`на ${schetchik} итерации число ${i}`)
+    schetchik++
+}
